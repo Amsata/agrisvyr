bf9b9383c87d41e2f06657eec1dd024c56ea314f
Amsata <amsata_niang@yahoo.fr>
2022-10-23 22:36:18 +0200

    update 23-10-2022

--- R/replace_missing.R
+++ R/replace_missing.R
@@ -8,15 +8,26 @@
 #' @return a dataframe
 #' @importFrom dplyr %>%
 #' @importFrom rlang :=
+#' @importFrom rlang ensym
 #' @export
 #'
 #' @examples
-replace_missing=function(data,var,replacement=999,label="structural Missing"){
-  if("haven_labelled" %in% (data %>% dplyr::pull({{var}}) %>% class())){
-    data %>% dplyr::mutate({{var}}:=tidyr::replace_na({{var}},replacement)) %>%
-      labelled::add_value_labels({{var}}:=c("structural Missing"=replacement))
-  }else {
-    data %>% dplyr::mutate({{var}}:=tidyr::replace_na(labelled::to_labelled({{var}}),replacement)) %>%
-      labelled::add_value_labels({{var}}:=c("structural Missing"=replacement))
+#'
+replace_missing = function(data,
+                           var,
+                           replacement = 999,
+                           label = "structural Missing") {
+
+  stopifnot(is.data.frame(data))
+  stopifnot(rlang::as_string(ensym(var)) %in% names(data))
+  stopifnot(is.integer(replacement))
+  stopifnot(is.character(label))
+
+  if ("haven_labelled" %in% (data %>% dplyr::pull({{var}}) %>% class())) {
+    data %>% dplyr::mutate({{var}} := tidyr::replace_na({{var}}, replacement)) %>%
+      labelled::add_value_labels({{var}} := c("structural Missing" = replacement))
+  } else {
+    data %>% dplyr::mutate({{var}} := tidyr::replace_na(labelled::to_labelled({{var}}), replacement)) %>%
+      labelled::add_value_labels({{var}} := c("structural Missing" = replacement))
   }
 }
