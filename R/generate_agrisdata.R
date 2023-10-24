721c00dd744af6bd0f674293a26ac131b38407b5
Amsata <amsata_niang@yahoo.fr>
2023-10-24 15:43:29 +0200

    Optimize the generation of agrisurvey data by creating a question object and a questionnaire that parametrizes the language.

--- R/generate_agrisdata.R
+++ R/generate_agrisdata.R
@@ -20,6 +20,7 @@ gen_names=function(sex,first_last){
 label_geo_name=function(data,label,level,agris_var){
   id_sym=sym(level)
   name_sym=sym(label)
+  agris_var_sym=sym(agris_var)
 
   df=data %>% st_drop_geometry() %>% dplyr::select(id:={{id_sym}},name:={{name_sym}}) %>%
     dplyr::distinct(id,name)
@@ -27,7 +28,7 @@ label_geo_name=function(data,label,level,agris_var){
   label_name=label_val_gen(df$name,df$id)
 
   data=data %>% label_name({{name_sym}}) %>% dplyr::select(-{{id_sym}}) %>%
-    dplyr::rename({{agris_var}}:={{name_sym}})
+    dplyr::rename({{agris_var_sym}}:={{name_sym}})
 
   return(data)
 }
@@ -70,4 +71,137 @@ stopifnot(language %in% c("en","fr"))
   }
 
 return(res)
+}
+
+
+#' Title
+#'
+#' @param shpfile
+#' @param level
+#' @param geo_id
+#' @param geo_name
+#' @param np
+#' @param na_prob
+#' @param language
+#'
+#' @return
+#' @export
+#'
+#' @examples
+generateAgrisData <- function(quest,shpfile, level=3,
+                               geo_id=c("ID_1","ID_2","ID_3"),
+                               geo_name=c("NAME_1","NAME_2","NAME_3"),
+                               np=30,na_prob=0.0005,lang="en"){
+
+  stopifnot(lang %in% c("en","fr","es"))
+
+  shpfile=shpfile %>% dplyr::select(all_of(c(geo_id,geo_name)))
+
+
+  if (level==1){
+    shpfile=shpfile %>% label_geo_name(geo_name[1],geo_id[1],quest$Q13a@QuestVar[[lang]]) %>%
+      dplyr::mutate(!!sym(quest$Q13b@QuestVar[[lang]]):=NA,
+                    !!(quest$Q13c@QuestVar[[lang]]):=NA)
+  }
+
+  if(level==2){
+    shpfile=shpfile %>% label_geo_name(geo_name[1],geo_id[1],quest$Q13a@QuestVar[[lang]])
+    shpfile=shpfile %>% label_geo_name(geo_name[2],geo_id[2],quest$Q13b@QuestVar[[lang]]) %>%
+      dplyr::mutate(!!sym(quest$Q13c@QuestVar[[lang]]):=NA)
+  }
+
+  if(level==3){
+    shpfile=shpfile %>% label_geo_name(geo_name[1],geo_id[1],quest$Q13a@QuestVar[[lang]])
+    shpfile=shpfile %>% label_geo_name(geo_name[2],geo_id[2],quest$Q13b@QuestVar[[lang]])
+    shpfile=shpfile %>% label_geo_name(geo_name[3],geo_id[3],quest$Q13c@QuestVar[[lang]])
+  }
+
+
+  points=shpfile %>%st_sample(shpfile, size = np:(np+1), type = "random") %>% sf::st_as_sf()
+  points_df=as.data.frame(st_coordinates(points))
+
+
+  final_points=points %>% sf::st_join(shpfile) %>%
+    bind_cols(points_df) %>% dplyr::rename(!!sym(quest$Q19a@QuestVar[[lang]]):=X,
+                                           !!sym(quest$Q19b@QuestVar[[lang]]):=Y) %>%
+    dplyr::mutate(!!sym(quest$Q17d@QuestVar[[lang]]):=!!sym(quest$Q13b@QuestVar[[lang]]),
+                  !!sym(quest$Q17c@QuestVar[[lang]]):=!!sym(quest$Q13a@QuestVar[[lang]]),
+                  !!sym(quest$Q17b@QuestVar[[lang]]):=!!sym(quest$Q13c@QuestVar[[lang]]))
+
+
+  data=final_points
+
+
+  # CREATING CATEGORICAL VARIABLES
+  for (q in seq_along(quest)) {
+    cat_vars=quest[[q]]@QuestType=="cat." & !is.null(quest[[q]]@QuestValProbs)
+    if(cat_vars) {
+      data=data %>% dplyr::mutate(
+        !!quest[[q]]@QuestVar[[lang]]:= wakefield::r_sample(n,x=quest[[q]]@QuestValue[[lang]],
+                                                            prob=quest[[q]]@QuestValProbs)
+      )
+    }
+  }
+
+
+  data=data %>% dplyr::mutate(
+
+    #*****************************************************************************
+    #* PART 1.2: IDENTIFICATION OF THE HOLDING ***********************************
+    #* ***************************************************************************
+    holdingID=wakefield::id(n)
+    #*****************************************************************************
+    #* PART 1.2: AGRICULTURAL ACTIVITY         ***********************************
+    #* ***************************************************************************
+
+  )   %>%
+    # skip controle
+
+    #*****************************************************************************
+    #* PART 1.2: IDENTIFICATION OF THE HOLDING ***********************************
+    #* ***************************************************************************
+    #If Q10 = 1 or 2 GO TO Q12, otherwise GO TO Q14
+    dplyr::mutate(
+    )  %>%
+    #generate enumeration area
+    dplyr::mutate(Region=ifelse(!is.na(!!quest$Q13a@QuestVar[[lang]]),!!quest$Q13a@QuestVar[[lang]],
+                                !!quest$Q17c@QuestVar[[lang]])) %>%
+    dplyr::group_by(Region) %>% dplyr::mutate(!!quest$Q15@QuestVar[[lang]]:=paste0(stringr::str_sub(Region,7),
+                                                         base::sample(100:120,1))) %>% dplyr::ungroup() %>%
+    dplyr::select(#-check,
+                  -Region) %>%
+    # perturbate by including some missing information
+    wakefield::r_na(prob = na_prob)
+
+
+  #LABELLING VARIABLES
+
+  for (q in seq_along(quest)) {
+    data=data %>% labelled::set_variable_labels(
+      !!quest[[q]]@QuestVar[[lang]]:= quest[[q]]@QuestLab[[lang]]
+    )
+  }
+
+  #LZBELLING VALUES
+
+  for (q in seq_along(quest)) {
+
+    if (!is.null(quest[[q]]@QuestValue[[lang]])) {
+
+      levels=seq_along(quest[[q]]@QuestValue[[lang]])
+      labels=quest[[q]]@QuestValue[[lang]]
+      names(levels)=labels
+
+      data=data %>% mutate(!!quest[[q]]@QuestVar[[lang]]:=to_labelled(
+        as.factor(as.character(!!quest[[q]]@QuestVar[[lang]])),
+        levels
+      ))
+
+    }
+
+  }
+
+
+  return(data)
+
 }
---
+++ R/questionnaire_object.R
@@ -0,0 +1,227 @@
+setClassUnion("numericOrNULL", c("numeric", "NULL"))
+setClassUnion("characterOrNULL", c("character", "NULL"))
+setClassUnion("logicalOrNULL", c("logical", "NULL"))
+setClassUnion("listOrNULL", c("list", "NULL"))
+setClassUnion("factorOrNULL", c("factor", "NULL"))
+setClassUnion("QuestValClass", c("factor","character","numeric","list", "NULL"))
+
+
+
+#' An object to model a survey question from Agris survey questionnaire
+#'
+#' @slot QuestVar name of the variable
+#' @slot QuestType type of the variable (categorical, continous or text)
+#' @slot QuestLab text of the question
+#' @slot QuestValue if categorical, take the different values of the question
+#' @slot QuestValProbs if categorical, take the probability of each values
+#'
+#' @return
+#' @export
+#'
+#' @examples
+methods::setClass(
+  Class          = "agrisvyQuestion",
+  representation = representation(
+    QuestVar      = "listOrNULL",
+    QuestType     = "characterOrNULL",
+    QuestLab      = "listOrNULL",
+    QuestValue    = "QuestValClass",
+    QuestValProbs = "numericOrNULL"
+  ),
+  prototype = prototype(
+    QuestVar      = NULL,
+    QuestType     = NULL,
+    QuestLab      = NULL,
+    QuestValue    = NULL,
+    QuestValProbs = NULL
+  ),
+  validity = function(object) {
+
+  }
+)
+
+
+#' Title
+#'
+#' @param QuestVar name of the variable
+#' @param QuestType type of the variable (categorical, continous or text)
+#' @param QuestLab text of the question
+#' @param QuestValue if categorical, take the different values of the question
+#' @param QuestValProbs if categorical, take the probability of each values
+#'
+#' @return
+#' @export
+#'
+#' @examples
+createQuestion <- function( QuestVar      = NULL,
+                           QuestType      = NULL,
+                           QuestLab       = NULL,
+                           QuestValue     = NULL,
+                           QuestValProbs  = NULL) {
+
+  obj <- new("agrisvyQuestion")
+
+  obj@QuestVar      = QuestVar
+  obj@QuestType     = QuestType
+  obj@QuestLab      = QuestLab
+  obj@QuestValue    = QuestValue
+  obj@QuestValProbs = QuestValProbs
+
+  return(obj)
+}
+
+
+
+#' Create a questionnaire object (liste of question objects)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+createQuestionnaire <- function(){
+
+  Questionnaire <- list()
+
+  #Q01
+  Questionnaire$Q01 <-
+    createQuestion(QuestVar   = list(en="Q01",fr="Q01",es="P01"),
+                   QuestType  = "cat.",
+                   QuestLab   = list(en="Did I find a farm at the same address or same name?",
+                                     fr="Ai-je trouvé une exploitation de même nom et/ou même adresse?",
+                                     es="¿Encontré	una	finca	en	la	misma	dirección	o	con	el	mismo	nombre?"),
+                   QuestValue = list(en=c("Yes","No"),fr=c("Oui","Non"),es=c("Si","No")),
+                   QuestValProbs = c(0.9,0.05)
+                   )
+
+  #Q01f
+  Questionnaire$Q01f <-
+    createQuestion(QuestVar =list(en="Q01f",fr="Q01f",es="P01f"),
+                   QuestType     ="cat.",
+                   QuestLab      =list(en="ationality",fr="Nationalité",es="Nacionalidad"),
+                   QuestValue    = list(en=c("Local country","Neighbouring country","Other"),
+                                        fr=c("Pays de résidence","Pays voisin","Autre"),
+                                        es=c("País	local","2 País	vecino","Otro")),
+                   QuestValProbs = c(0.8,0.2,0.1)
+                   )
+
+
+  Questionnaire$Q10 <-
+    createQuestion(QuestVar = list(en="Q10",fr="Q10",es="P10"),
+                   QuestType = "cat.",
+                   QuestLab = list(en="What is the legal status of the Holder?",
+                                   fr="Quel est le statut légal de l'exploitant?",
+                                   es="¿Cuál	es	la	condición	jurídica	del	Titular?"),
+                   QuestValue = list(en=c("Civil person/natural person",
+                                          "Group of civil persons/natural persons",
+                                                             "Legal person"),
+                                     fr=c("Personne physique","Groupe de personnes physiques",
+                                                             "Personne morale"),
+                                     es=c(" Persona	civil/persona	natural",
+                                          " Grupo	de	personas	civiles/personas	naturales",
+                                                             "Persona	jurídica")),
+                   QuestValProbs = c(0.9,0.2,0.01)
+                   )
+
+  #Q13a
+  Questionnaire$Q13a <-
+    createQuestion(QuestVar = list(en="Q13a",fr="Q13a",es="P13a"),
+                   QuestType = "cat.",
+                   QuestLab = list(en="Region",fr="Région",es="Región"),
+                   QuestValue = NULL,
+                   QuestValProbs = NULL
+                   )
+
+  #Q13b
+  Questionnaire$Q13b <-
+    createQuestion(QuestVar = list(en="Q13b",fr="Q13b",es="P13b"),
+                   QuestType = "cat.",
+                   QuestLab = list(en="District",fr="District",es="Distrito"),
+                   QuestValue = NULL,
+                   QuestValProbs = NULL
+                   )
+
+  #Q13c
+  Questionnaire$Q13c <-
+    createQuestion(QuestVar = list(en="Q13c",fr="Q13c",es="P13c"),
+                   QuestType = "cat.",
+                   QuestLab = list(en="Village or town name",
+                                   fr="Nom du village ou ville",
+                                   es=" Nombre	del	pueblo	o	ciudad"),
+                   QuestValue = NULL,
+                   QuestValProbs = NULL
+                   )
+
+  #Q15
+  Questionnaire$Q15 <-
+    createQuestion(QuestVar = list(en="Q15",fr="Q15",es="P15"),
+                   QuestType = "cont.",
+                   QuestLab = list(en=". Enumeration area of the holding",
+                                   fr="5. Zone de recensement de l'exploitation",
+                                   es="Área	de	enumeración	de	la	unidad	de	prod"),
+                   QuestValue = NULL,
+                   QuestValProbs = NULL
+                   )
+
+
+  # #Q17a
+  # Questionnaire$Q17a <-
+  #   createQuestion(QuestVar = list(en="Q17a",fr="Q17a",es="P17a"),
+  #                  QuestType = "cat.",
+  #                  QuestLab = list(en="a Address (street)",
+  #                                  fr="a Adresse (rue)",
+  #                                  es="Dirección	(calle)"),
+  #                  QuestValue = NULL,
+  #                  QuestValProbs = NULL
+  #                  )
+
+  #Q17b
+  Questionnaire$Q17b <-
+    createQuestion(QuestVar = list(en="Q17b",fr="Q17b",es="P17b"),
+                   QuestType = "cat.",
+                   QuestLab = list(en="Village or town name",
+                                   fr="Nom du village ou ville",
+                                   es=" Nombre	del	pueblo	o	ciudad"),
+                   QuestValue = NULL,
+                   QuestValProbs = NULL
+                   )
+
+  #Q17c
+  Questionnaire$Q17c <-
+    createQuestion(QuestVar = list(en="Q17c",fr="Q17c",es="P17c"),
+                   QuestType = "cat.",
+                   QuestLab = list(en="Region",fr="Région",es="Región"),
+                   QuestValue = NULL,
+                   QuestValProbs = NULL
+                   )
+
+  #Q17d
+  Questionnaire$Q17d <-
+    createQuestion(QuestVar = list(en="Q17d",fr="Q17d",es="P17d"),
+                   QuestType = "cat.",
+                   QuestLab = list(en="District",fr="District",es="Distrito"),
+                   QuestValue = NULL,
+                   QuestValProbs = NULL
+                   )
+
+  #Q19a
+  Questionnaire$Q19a <-
+    createQuestion(QuestVar = list(en="Q19a",fr="Q19a",es="P19a"),
+                   QuestType = "cont.",
+                   QuestLab = list(en="Latitude",fr="Latitude",es="Latitud"),
+                   QuestValue = NULL,
+                   QuestValProbs = NULL
+                   )
+  #Q19b
+  Questionnaire$Q19b <-
+    createQuestion(QuestVar = list(en="Q19b",fr="Q19b",es="P19b"),
+                   QuestType = "cont.",
+                   QuestLab = list(en="Longitude",fr="Longitude",es="Longitud"),
+                   QuestValue = NULL,
+                   QuestValProbs = NULL
+                   )
+
+
+
+return(Questionnaire)
+
+}
